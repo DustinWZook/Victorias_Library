2015dba371cbc4fc234aadd84d7145acb3675c66
Dustin Zook <dustin.w.zook@gmail.com>
2026-02-04 15:43:26 -0600

    testing fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+// import './App.css';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./components/Layout";
+//
+// import LandingPage from "./pages/LandingPage";
+// import FriendsPage from "./pages/FriendsPage";
+// import BookDetailsPage from "./pages/BookDetailsPage";
+// import LibraryPage from "./pages/LibraryPage";
+// import LoansPage from "./pages/LoansPage";
+//
+//
+// function App() {
+//   return (
+//     <BrowserRouter>
+//         <Routes>
+//             <Route path="/" element={<Layout />}>
+//                 <Route index element={<LandingPage />}></Route>
+//                 <Route path="/library" element={<LibraryPage />}></Route>
+//                 <Route path="/loans" element={<LoansPage />}></Route>
+//                 <Route path="/friends" element={<FriendsPage />}></Route>
+//                 <Route path="/book/:id" element={<BookDetailsPage />}></Route>
+//             </Route>
+//         </Routes>
+//     </BrowserRouter>
+//   )
+// }
+//
+// export default App
 
-import LandingPage from "./pages/LandingPage";
-import FriendsPage from "./pages/FriendsPage";
-import BookDetailsPage from "./pages/BookDetailsPage";
-import LibraryPage from "./pages/LibraryPage";
-import LoansPage from "./pages/LoansPage";
 
-
-function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<LandingPage />}></Route>
-                <Route path="/library" element={<LibraryPage />}></Route>
-                <Route path="/loans" element={<LoansPage />}></Route>
-                <Route path="/friends" element={<FriendsPage />}></Route>
-                <Route path="/book/:id" element={<BookDetailsPage />}></Route>
-            </Route>
-        </Routes>
-    </BrowserRouter>
-  )
+export default function App() {
+    return (
+        <div style={{ padding: 24 }}>
+            <h1>App loaded</h1>
+            <p>If you see this, React is mounting.</p>
+        </div>
+    );
 }
-
-export default App
